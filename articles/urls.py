32595b6295c4896e5bccf428b4dc2def502b2dc6
Octowl <alsaff1987@gmail.com>
2018-03-04 10:07:23 +0300

    Allow for unicode url parameters

--- articles/urls.py
+++ articles/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.people, name="people"),
     path('add_person/', views.add_person, name="add_person"),
-    path('update_person/<slug:person_slug>', views.update_person, name="update_person"),
-    path('<slug:person_slug>', views.person, name="person"),
+    path('update_person/<person_slug>', views.update_person, name="update_person"),
+    path('delete_person/<person_id>', views.delete_person, name="delete_person"),
+    path('<person_slug>', views.person, name="person"),
 ]
